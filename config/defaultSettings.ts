4b2ada9039c5a13e9ae79cf063569ebc3e2f4270
zhangli <zhangli@flashhold.com>
2021-10-28 14:40:00 +0800

    国际化删除部分国家

--- config/defaultSettings.ts
+++ config/defaultSettings.ts
@@ -12,9 +12,8 @@ const Settings: LayoutSettings & {
   fixedHeader: false,
   fixSiderbar: true,
   colorWeak: false,
-  title: 'Ant Design Pro',
+  title: 'welcome',
   pwa: false,
-  logo: 'https://gw.alipayobjects.com/zos/rmsportal/KDpgvguMpGfqaHPjicRK.svg',
   iconfontUrl: '',
 };
 
